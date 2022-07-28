52b8518ed34efa98bf0871a85d3dd9c727cd45d8
Mahtab Hossen Jhalok <48786676+Mahtabhj@users.noreply.github.com>
2022-07-28 20:23:09 +0600

    Second commit

--- status/api/index.js
+++ status/api/index.js
@@ -2,15 +2,12 @@ const express = require("express");
 const app = express();
 
 const router = require("express").Router();
-const User = require("./models/User");
-const Hostel = require("./models/Hostel");
+
 
 const dotenv = require("dotenv");
 const mongoose = require("mongoose")
-const hostelRoute = require("./routes/hostels");
-const houseRoute = require("./routes/houses");
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users");
+
+const http = require("http").createServer(app)
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
@@ -24,7 +21,7 @@ app.use(express.urlencoded({extended: true}));
 
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
-mongoose.connect('mongodb://localhost:27017/Assignment', {
+mongoose.connect('mongodb://localhost:27017/Status', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 }).then(() => {
@@ -35,95 +32,32 @@ mongoose.connect('mongodb://localhost:27017/Assignment', {
 });
 
 
-var Minio = require('minio')
-
-var minioClient = new Minio.Client({
-    endPoint: '127.0.0.1',
-    port: 9000,
-    useSSL: false,
-    accessKey: 'ILdloFV62IoAlMOV',
-    secretKey: 'bKSQ8DzAZO1wsP5Q9kikl8IBHMAXigZz'
-});
-
-minioClient.bucketExists('mybucket', function(err, exists) {
-  if (err) {
-    return console.log(err)
-  }
-  if (exists) {
-    return console.log('Bucket exists.')
-  }
-})
-
-
-var globalVariable={
-  uuid: crypto.randomUUID()
-};
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-       cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-
-    cb(null, file.originalname);
- 
-  },
-});
-
-const upload = multer({storage: storage });
-
-app.post("/api/upload", upload.single('file'), (req, res) => {
-
-    
-  const directoryPath = path.join('./images/',req.file.filename);
-  
-console.log(req.file.filename)
-  minioClient.fPutObject('mybucket', req.file.filename, directoryPath, function(err, objInfo) {
-    if(err) {
-        return console.log(err)
-    }
-    console.log("Success", objInfo.etag, objInfo.versionId)
-})
-  res.send(req.file)
-}, (error, req, res, next) => {
-
-  
-
-  console.log(req.file)
- 
-  res.status(400).send({ error: error.message })
-})
-
-
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-app.use("/api/hostels", hostelRoute);
-app.use("/api/houses", houseRoute);
-app.use("/api/categories", categoryRoute);
+app.use("/api/categories",categoryRoute);
 
-app.listen("8001", () => {
-    console.log("Status Backend is running on 8001");
-  });
 
-  const videoStorage = multer.diskStorage({
-    destination: 'images', // Destination to store video 
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname))
-    }
-});
+app.use('/',(req,res,next) => {
 
-const videoUpload = multer({
-    storage: videoStorage,
-    limits: {
-        fileSize: 10000000   // 10000000 Bytes = 10 MB
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(mp4|MPEG-4)$/)) {     // upload only mp4 and mkv format
-            return cb(new Error('Please upload a Video'))
-        }
-        cb(undefined, true)
-    }
+  return res.status(200).json({"msg":"Hello from status"})
+
 })
+app.use(
+  express.urlencoded(
+      {
+          extended: true 
+      }
+  )
+)
+// app.listen("8001", () => {
+//     console.log("Status Backend is running on 8001");
+//   });
+
+http.listen(8001,()=>{
+  console.log('Listening on port 8001')
+})
+
+
+ 
 
 
 
